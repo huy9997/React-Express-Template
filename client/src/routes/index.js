c107ee9bf5b0d801d7fd95fde6409a9bcdeeef52
Huy Nguyen <huy9997@gmail.com>
2019-05-02 10:41:05 -0700

    facebook login complete for basic template

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,14 @@
 import React from  'react'
 import {Route,BrowserRouter } from 'react-router-dom'
 import HelloWorld from '../screens/helloWorld'
+import FacebookLogin from '../screens/LoginFacebook'
 
 const Routes =()=>(
   <BrowserRouter>
       <Route exact path="/" component={HelloWorld}/>
+      <Route exact path="/Login" component={FacebookLogin}/>
+
+
   </BrowserRouter>
 )
 
